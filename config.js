82d14d79acc95dc672584a3c7e779afb6778e374
jdtalley <dakota.talley@gmail.com>
2019-08-01 02:29:28 +0000

    Added config file for env variables. Updated Dependencies.

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+const config = {
+	app: {
+		port: 8081
+	},
+	db: {
+		host: 'yelpcamp-hgmvz.mongodb.net/test?retryWrites=true&w=majority',
+		port: '',
+		name: ''
+	}
+};
+
+module.exports = config;
